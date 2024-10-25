90f80e855cf96f9843a7cc81e84ab87b3be40be4
swdipple <swdipple@gmail.com>
2024-10-25 17:47:15 -0400

    Fixed numerous bugs with the emotion example. Now produce a correct xml file.

--- Python_Construct_Input_Builder/emotion_example.py
+++ Python_Construct_Input_Builder/emotion_example.py
@@ -55,7 +55,7 @@ emot_reading = [
     [4.0,	-0.8,	-0.8,	-0.8,	1.0,	5.0]    #hap
 ]
 
-deltat = 0.5
+deltat = 0.1
 total_time = 96.0
 
 agent_count = 100
@@ -64,7 +64,7 @@ time_count = int(total_time / deltat)
 reading_scale = 0.0075
 
 construct_xml = cb.ConstructBuilder()
-construct_xml.add_parameter("verbose runtime", "true")
+construct_xml.add_construct_parameter("verbose runtime", "true")
 
 time_ns = construct_xml.add_nodeset("time")
 for i in range(time_count):
@@ -94,12 +94,12 @@ for i in range(agent_count):
 construct_xml.add_network("knowledge network", "bool", "agent", "knowledge", src_rep="sparse", trg_rep="sparse", default_value="true")
 emot_net = construct_xml.add_network("emotion network", "float", "agent", "emotion")
 for agent_index in range(agent_count):
-    for emot_index, attractor in attractors.enumerate():
-        construct_xml.add_network_link(emot_net, agent_index, emot_index, value = np.random.uniform(-.1, .1) + attractors)
+    for emot_index, attractor in enumerate(attractors):
+        construct_xml.add_network_link(emot_net, agent_index, emot_index, value = np.random.uniform(-.1, .1) + attractor)
 
 emot_bc_bias = construct_xml.add_network("emotion broadcast bias network", "float", "agent", "emotion")
 for agent_index in range(agent_count):
-    for emot_index, value in emot_broadcast_probabilities.enumerate():
+    for emot_index, value in enumerate(emot_broadcast_probabilities):
         construct_xml.add_network_link(emot_bc_bias, agent_index, emot_index, value = value)
 construct_xml.add_network("emotion broadcast first order network", "float", "emotion", "emotion", src_rep="sparse", trg_rep="sparse")
 
@@ -109,33 +109,33 @@ for (row, col), value in np.ndenumerate(emot_reading):
 
 emot_attractors_net = construct_xml.add_network("emotion attractors network", "float", "agent", "emotion")
 for agent_index in range(agent_count):
-    for emot_index, value in attractors.enumerate():
+    for emot_index, value in enumerate(attractors):
         construct_xml.add_network_link(emot_attractors_net, agent_index, emot_index, value = value)
         
 emot_decay_rate_net = construct_xml.add_network("emotion regulation network", "float", "agent", "emotion")
 for agent_index in range(agent_count):
-    for emot_index, value in decay_rates.enumerate():
+    for emot_index, value in enumerate(decay_rates):
         construct_xml.add_network_link(emot_decay_rate_net, agent_index, emot_index, value = value * deltat)
 
 activity_ns = construct_xml.add_nodeset("activity")
-for act, info in activities.items():
-    activity_node = construct_xml.add_node(activity_ns, act)
-    if "transform" in info:
-        construct_xml.add_node_attribute(activity_node, {"enable emotional dependence": info["transform"]})
+for act in activities:
+    activity_node = construct_xml.add_node(activity_ns, act["name"])
+    if "transform" in act:
+        construct_xml.add_node_attribute(activity_node, {"enable emotional dependence": act["transform"]})
         
 activity_net = construct_xml.add_network("agent activity weights network", "float", "agent", "activity")
 base_activity = construct_xml.add_network("base activity rate network", "float", "agent", "activity")
 
-for index, info in activities.enumerate():
+for index, info in enumerate(activities):
     if "transform" in info:
         for agent_index in range(agent_count):
             construct_xml.add_network_link(base_activity, agent_index, index, value=info["base"])
-        if "first order" in info and info["first order"] != None:
+        if "first order" in info and info["first order"] is not None:
             net = construct_xml.add_network("first order " + info["name"] + " emotion network", "float", "agent", "emotion")
             for agent_index in range(agent_count):
-                for emot, value in info["first order"].enumerate():
+                for emot, value in enumerate(info["first order"]):
                     construct_xml.add_network_link(net, agent_index, emot, value=value)
-        if "second order" in info and info["second order"] != None:
+        if "second order" in info and info["second order"] is not None:
             net = construct_xml.add_network("second order " + info["name"] + " emotion network", "float", "agent", "emotion", "emotion")
             for agent_index in range(agent_count):
                 for (row, col), value in np.ndenumerate(info["second order"]):
